ffdb6922e62ba6f428c9259a18359bc91388e5e1
shuaijie <shuaijie@amazon.com>
2019-11-26 22:55:47 -0800

    finish the project

---
+++ src/main/command/lifecycledemo.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+java -cp ${project.build.finalName}.jar org.sonatype.mavenbook.ch07.App $*
--- src/main/java/org/sonatype/mavenbook/ch07/App.java
+++ src/main/java/org/sonatype/mavenbook/ch07/App.java
@@ -2,12 +2,14 @@ package org.sonatype.mavenbook.ch07;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+
+        String msg = "Hello World!";
+        if (args.length > 0) {
+            msg = args[0];
+        }
+        System.out.println(msg);
     }
 }
